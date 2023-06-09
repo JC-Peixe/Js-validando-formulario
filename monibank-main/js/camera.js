cc1dc641eb80a3e5b605946793c2fee2fac8ccc7
Peixee <Jc_px@hotmail.com>
2023-06-08 22:59:19 -0300

    Validando Formularios com JS Concluido
    projeto do curso da alura, feito junto com a professora durante o curso...

---
+++ monibank-main/js/camera.js
@@ -0,0 +1,24 @@
+const botaoIniciarCamera = document.querySelector('[data-video-botao]');
+const campoCamera = document.querySelector('[data-camera]');
+const video = document.querySelector ('[data-video]');
+const botaTirarFoto = document.querySelector('[data-tirar-foto]');
+const canvas = document.querySelector('[data-video-canvas]');
+const mensagem = document.querySelector('[data-mensagem]');
+let imageURL = '';
+
+botaoIniciarCamera.addEventListener('click', async function (){
+    const iniciaVideo = await navigator.mediaDevices
+    .getUserMedia({video:true, audio:false})
+
+    botaoIniciarCamera.style.display = 'none';
+    campoCamera.style.display = 'block';
+
+    video.srcObject = iniciaVideo;
+})
+
+botaTirarFoto.addEventListener('click', function(){
+    canvas.getContext('2d').drawImage(video,0,0,canvas.width,canvas.heigth);
+    imageURL = canvas.toDataURL('image/jpeg');
+    campoCamera.style.display = "none";
+    mensagem.style.display = 'block';
+})
