14ae783733cc8ffa8a54c28685e39ff916ff5598
Peixee <Jc_px@hotmail.com>
2023-06-09 15:56:59 -0300

    Update camera.js

--- monibank-main/js/camera.js
+++ monibank-main/js/camera.js
@@ -4,6 +4,8 @@ const video = document.querySelector ('[data-video]');
 const botaTirarFoto = document.querySelector('[data-tirar-foto]');
 const canvas = document.querySelector('[data-video-canvas]');
 const mensagem = document.querySelector('[data-mensagem]');
+const botaoEnviarFoto = document.querySelector("[data-enviar]");
+
 let imageURL = '';
 
 botaoIniciarCamera.addEventListener('click', async function (){
@@ -18,7 +20,20 @@ botaoIniciarCamera.addEventListener('click', async function (){
 
 botaTirarFoto.addEventListener('click', function(){
     canvas.getContext('2d').drawImage(video,0,0,canvas.width,canvas.heigth);
+
     imageURL = canvas.toDataURL('image/jpeg');
+    
     campoCamera.style.display = "none";
     mensagem.style.display = 'block';
+})
+
+botaoEnviarFoto.addEventListener('click', () => {
+    const receberDadosExistentes = localStorage.getItem("cadastro");
+    const converteRetorno = JSON.parse(receberDadosExistentes);
+
+    converteRetorno.imagem = imagemURL;
+
+    localStorage.setItem('cadastro', JSON.stringify(converteRetorno))
+
+    window.location.href = '../pages/abrir-conta-form-3.html';
 })
